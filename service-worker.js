d5a863efef297ce89c67b927a0d2d1d6620aac4f
jp-1990 <plum_1990@hotmail.co.uk>
2020-08-18 23:13:09 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb4abe614242d4bf457d513f200e76d3.js"
+  "/precache-manifest.5a0f8b7e324e36eea01818afdee4bd47.js"
 );
 
 self.addEventListener('message', (event) => {
