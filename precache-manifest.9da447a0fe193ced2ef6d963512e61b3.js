30eb31551b5d533d3428cd6fbb04be31377af559
jp-1990 <plum_1990@hotmail.co.uk>
2020-08-18 20:19:02 +0100

    Updates

---
+++ precache-manifest.9da447a0fe193ced2ef6d963512e61b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bca440f293375af028b5ce95bd7d6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "144b54fcca0485796f4f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0971630519eddb3beaf1",
+    "url": "/static/js/2.4e1eb000.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e1eb000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144b54fcca0485796f4f",
+    "url": "/static/js/main.5c482738.chunk.js"
+  },
+  {
+    "revision": "cc648eb61acfa73ff313",
+    "url": "/static/js/runtime-main.84f8ba11.js"
+  }
+]);
