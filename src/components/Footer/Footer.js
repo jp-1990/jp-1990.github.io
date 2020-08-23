f07ac1f3dc2af07cdc093eed8927eb61b51194e6
jp-1990 <plum_1990@hotmail.co.uk>
2020-08-23 20:15:36 +0100

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+const footer = () => {
+  return (
+    <div className={classes.Footer}>
+      <p> © by J. Plummer. All rights reserved.</p>
+    </div>
+  );
+};
+export default footer;
