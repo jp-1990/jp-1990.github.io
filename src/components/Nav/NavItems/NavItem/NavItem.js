8df2e46ed93b8fa8e479581e43f276c399f8a58c
jp-1990 <plum_1990@hotmail.co.uk>
2020-08-30 15:49:14 +0100

    responsive nav-bar

--- src/components/Nav/NavItems/NavItem/NavItem.js
+++ src/components/Nav/NavItems/NavItem/NavItem.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import classes from './NavItem.module.css';
 
-// window.scroll({
-//   left: 0,
-//   top: document.getElementById('Projects').offsetTop - 60,
-//   behavior: 'smooth',
-// });
-
 const navItem = (props) => {
   const scrollHandler = () => {
     window.scroll({
@@ -16,10 +10,8 @@ const navItem = (props) => {
     });
   };
   return (
-    <li className={classes.NavItem}>
-      <p className={classes[props.children]} onClick={scrollHandler}>
-        {props.children}
-      </p>
+    <li className={classes.NavItem} onClick={scrollHandler}>
+      <p className={classes[props.children]}>{props.children}</p>
     </li>
   );
 };
--- src/components/Nav/Toolbar/Toolbar.js
+++ src/components/Nav/Toolbar/Toolbar.js
@@ -4,10 +4,14 @@ import classes from './Toolbar.module.css';
 
 const Toolbar = () => {
   const [scrolled, setScrolled] = useState(false);
-  const [widthHeight, setWidthHeight] = useState();
+  const [widthHeight, setWidthHeight] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
   const [projectsY, setProjectsY] = useState();
   const [aboutY, setAboutY] = useState();
   const [skillsY, setSkillsY] = useState();
+  const [menuState, setMenuState] = useState('closed');
 
   useEffect(() => {
     const handleResize = () => {
@@ -24,32 +28,82 @@ const Toolbar = () => {
   }, [widthHeight]);
 
   useEffect(() => {
-    let result;
     window.addEventListener('scroll', () => {
       const display =
         document.querySelector('main').getBoundingClientRect().y < 68;
 
       if (display !== true) {
-        result = false;
+        setScrolled(false);
+        setMenuState('closed');
       } else {
-        result = true;
+        setScrolled(true);
       }
-      setScrolled(result);
     });
     return () => {};
   }, [scrolled]);
 
-  return (
-    <header>
-      <div className={scrolled ? classes.Toolbar : classes.Hide}>
-        <nav>
-          <NavItems
-            yValues={{ projects: projectsY, about: aboutY, skills: skillsY }}
-          />
-        </nav>
-      </div>
-    </header>
-  );
+  const toggleMenuHandler = () => {
+    if (menuState === 'closed') {
+      setMenuState('open');
+      document.getElementById('toolbar').classList.remove(classes.closed);
+      document.getElementById('toolbar').classList.add(classes.open);
+    } else {
+      setMenuState('closed');
+      document.getElementById('toolbar').classList.remove(classes.open);
+      document.getElementById('toolbar').classList.add(classes.closed);
+    }
+  };
+
+  const output = () => {
+    if (widthHeight[0] < 426) {
+      return (
+        <div
+          id='toolbar'
+          className={
+            scrolled
+              ? `${classes.Toolbar} ${classes.Default}`
+              : `${classes.Toolbar} ${classes.Hide}`
+          }
+        >
+          <div className={scrolled ? classes.Menu : classes.None}>
+            <div className={classes.Container} onClick={toggleMenuHandler}>
+              <div
+                className={
+                  menuState === 'open'
+                    ? `${classes.Hamburger} ${classes.MenuOpen}`
+                    : classes.Hamburger
+                }
+              ></div>
+            </div>
+          </div>
+          <nav
+            className={menuState === 'open' ? null : classes.None}
+            onClick={toggleMenuHandler}
+          >
+            <NavItems
+              yValues={{
+                projects: projectsY,
+                about: aboutY,
+                skills: skillsY,
+              }}
+            />
+          </nav>
+        </div>
+      );
+    } else {
+      return (
+        <div className={scrolled ? classes.Toolbar : classes.Hide}>
+          <nav>
+            <NavItems
+              yValues={{ projects: projectsY, about: aboutY, skills: skillsY }}
+            />
+          </nav>
+        </div>
+      );
+    }
+  };
+
+  return <header>{output()}</header>;
 };
 
 export default Toolbar;
